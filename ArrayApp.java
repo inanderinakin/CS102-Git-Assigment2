c797270b3dc046affcd62553fdd7a2f68840a2d3
Yiğit Türk <yigit.turk@ug.bilkent.edu.tr>
2026-01-27 22:44:17 +0300

    The ArrayApp class is implemented

---
+++ ArrayApp.java
@@ -0,0 +1,10 @@
+//**
+// The ArrayApp class manages the running of the Array Application.
+//  */
+class ArrayApp {
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+        menu.runMenu();
+    }
+}
