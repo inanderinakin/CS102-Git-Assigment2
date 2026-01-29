15ed3bef85939255c7d31eb258117025fe76aa34
İnan Derin Akın <inandakin@gmail.com>
2026-01-29 08:01:19 +0300

    Added comments line to ArrayCreator file for improved clarity

--- ArrayCreator.java
+++ ArrayCreator.java
@@ -1,9 +1,13 @@
 import java.util.Random;
 
 public class ArrayCreator {
+    // This method creates an array with given size and fills it with random integers from 0 to 100 
     public static int[] createArray(int size) {
+        // Creating the objects
         Random random = new Random();
         int[] arr = new int[size];
+
+        // For every element in the array, replace it with a random integer from 0 to 100
         for (int i = 0; i < size; i++) {
             arr[i] = random.nextInt(0, 100);
         }
