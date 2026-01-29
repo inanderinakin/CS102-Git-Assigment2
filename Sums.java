1ceec7c61218639ae3c9376f9c4e859f99c89d8e
enes <ccetineneseren@gmail.com>
2026-01-29 10:08:17 +0300

    Modified the class to return the sums with 2 methods.

--- Sums.java
+++ Sums.java
@@ -1,20 +1,30 @@
 public class Sums{
 
-    public static void SumOfIndexes(int[] numbers){
+    // calculates sum of numbers at even indexes and returns it
+    public static int sumOfEvenIndexes(int[] numbers){
 
-        // both sums are 0 in the beginning
-        int sumOfOddIndexes = 0;
+        // sum is 0 initially
         int sumOfEvenIndexes = 0;
 
         for (int i = 0; i < numbers.length; i++){   // loop goes on for 'size of array' times
             if (i % 2 == 0) {                       // checks if the index is divisible by 2 (if it is then the index is even)
                 sumOfEvenIndexes += numbers[i];     // adds the number at that index to the sum of even indexes
-            }else{                                  // if it is not then the index is odd
-                sumOfOddIndexes += numbers[i];      // adds the number at that index to the sum of odd indexes
+            }
+            }
+        return sumOfEvenIndexes;
+    }
+
+    // calculates sum of numbers at odd indexes and returns it
+    public static int sumOfOddIndexes(int[] numbers){
+
+        // sum is 0 initially
+        int sumOfOddIndexes = 0;
+
+        for (int i = 0; i < numbers.length; i++){   // loop goes on for 'size of array' times
+            if (i % 2 == 1) {                       // checks if the index is divisible by 2 (if it is not then the index is odd)
+                sumOfOddIndexes += numbers[i];     // adds the number at that index to the sum of odd indexes
             }
         }
-
-        System.out.println("Sum of even indexes: " + sumOfEvenIndexes);
-        System.out.println("Sum of odd indexes: " + sumOfOddIndexes);
+        return sumOfOddIndexes;
     }
 }
